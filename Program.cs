1dc9213d20c7e10477b99ceb9a9e8b829eef00db
Eda Öney <oneeyyeda@gmail.com>
2025-07-28 19:11:43 +0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ortalama Hesaplama");
+
+double toplam = 0;
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"Lütfen {i}. sayıyı  giriniz: ");
+    double number = double.Parse(Console.ReadLine());
+    toplam += number;
+}
+double ortalama = toplam / 5;
+Console.WriteLine($"Girilen sayıların ortalaması: {ortalama}");
